f0ecf0ad8e5ad3ad10ec4e9a3de1778f216dd1aa
Christian Gördes <christian.goerdes@outlook.de>
2023-04-25 21:39:30 +0200

    added doxygen comments

--- include/singlyLinkedList/listNode.hpp
+++ include/singlyLinkedList/listNode.hpp
@@ -5,22 +5,47 @@
 
 template<class T>
 
+/**
+ * @brief Node class for a singly linked list
+ * @tparam T Data type to be stored in the node
+ */
 class ListNode {
 
 public:
+
+    /**
+     * @brief Constructor for the ListNode class
+     * @param data Data to be stored in the node
+     */
     ListNode(T data);
 
-    // Getter
+    /**
+     * @brief Returns a shared_ptr to the next node in the list
+     * @return Shared_ptr to the next node
+     */
     std::shared_ptr<ListNode<T>> get_next();
+
+    /**
+     * @brief Returns the data stored in the node
+     * @return Data stored in the node
+     */
     T get_data();
 
-    // Setter
+    /**
+     * @brief Sets the next node in the list
+     * @param nextNode Shared_ptr to the next node in the list
+     */
     void set_next(std::shared_ptr<ListNode<T>> nextNode);
+
+    /**
+     * @brief Sets the data stored in the node
+     * @param newData Data to be stored in the node
+     */
     void set_data(T newData);
 
 private:
-    T data;
-    std::shared_ptr<ListNode<T>> next;
+    T data; /**< Data stored in the node */
+    std::shared_ptr<ListNode<T>> next; /**< Shared_ptr to the next node in the list */
 };
 
 #endif //PROJEKTALPHA_LISTNODE_HPP
