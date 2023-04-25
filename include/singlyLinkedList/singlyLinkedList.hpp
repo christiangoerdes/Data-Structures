88bbf20887d611b00e0f406bcfb89c7983ef8606
Christian Gördes <christian.goerdes@outlook.de>
2023-04-25 18:51:13 +0200

    added singlyLinkedList.hpp

---
+++ include/singlyLinkedList/singlyLinkedList.hpp
@@ -0,0 +1,14 @@
+#ifndef PROJEKTALPHA_SINGLYLINKEDLIST_HPP
+#define PROJEKTALPHA_SINGLYLINKEDLIST_HPP
+
+#include "listNode.hpp"
+
+class AbstractSinglyLinkedList {
+
+};
+
+class SinglyLinkedList : AbstractSinglyLinkedList {
+
+};
+
+#endif //PROJEKTALPHA_SINGLYLINKEDLIST_HPP
