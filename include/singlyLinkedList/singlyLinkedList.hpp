22e72a148512a412c40b9a9faa54e69b0bc86297
Christian Gördes <christian.goerdes@outlook.de>
2023-04-25 20:41:47 +0200

    added the singlyLinkedList header (Note: Iterator not included)

--- include/singlyLinkedList/singlyLinkedList.hpp
+++ include/singlyLinkedList/singlyLinkedList.hpp
@@ -21,6 +21,19 @@ template<class T>
 
 class SinglyLinkedList : public AbstractSinglyLinkedList<T> {
 
+public:
+    size_t get_size() = 0;
+    void get_front() = 0;    //TODO make get_front() return an Iterator
+    void insert_front(T t) = 0;
+    void insert_after(ListNode<T> predecessor, T t) = 0;
+    void remove_front() = 0;
+    void remove_after(ListNode<T> predecessor) = 0;
+
+private:
+    std::shared_ptr<ListNode<T>> head;
+    size_t size;
+    std::shared_ptr<ListNode<T>> get_head();
+    void set_head(std::shared_ptr<ListNode<T>> newHead);
 };
 
 #endif //PROJEKTALPHA_SINGLYLINKEDLIST_HPP
