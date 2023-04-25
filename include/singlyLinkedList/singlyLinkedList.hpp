6ce344928551aa63b504b3718fab61a680fb6537
Christian Gördes <christian.goerdes@outlook.de>
2023-04-25 20:36:34 +0200

    added the singlyLinkedList Interface (AbstractSinglyLinkedList)

--- include/singlyLinkedList/singlyLinkedList.hpp
+++ include/singlyLinkedList/singlyLinkedList.hpp
@@ -2,12 +2,24 @@
 #define PROJEKTALPHA_SINGLYLINKEDLIST_HPP
 
 #include "listNode.hpp"
+#include <iterator>
+
+template<class T>
 
 class AbstractSinglyLinkedList {
 
+public:
+    virtual size_t get_size() = 0;
+    virtual void get_front() = 0;    //TODO make get_front() return an Iterator
+    virtual void insert_front(T t) = 0;
+    virtual void insert_after(ListNode<T> predecessor, T t) = 0;
+    virtual void remove_front() = 0;
+    virtual void remove_after(ListNode<T> predecessor) = 0;
 };
 
-class SinglyLinkedList : AbstractSinglyLinkedList {
+template<class T>
+
+class SinglyLinkedList : public AbstractSinglyLinkedList<T> {
 
 };
 
