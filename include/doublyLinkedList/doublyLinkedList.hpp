e5514603054ab5906c0296018d5cd35185de8023
Christian Gördes <christian.goerdes@outlook.de>
2023-04-26 22:32:02 +0200

    implemented the interface for the doublyLinkedList

--- include/doublyLinkedList/doublyLinkedList.hpp
+++ include/doublyLinkedList/doublyLinkedList.hpp
@@ -1,8 +1,25 @@
-//
-// Created by Chrisi on 26.04.2023.
-//
+#pragma once
 
-#ifndef PROJEKTALPHA_DOUBLYLINKEDLIST_HPP
-#define PROJEKTALPHA_DOUBLYLINKEDLIST_HPP
+#include "listNode.hpp"
+#include <iterator>
 
-#endif //PROJEKTALPHA_DOUBLYLINKEDLIST_HPP
+
+template <typename T>
+class AbstractDoublyLinkedList {
+
+public:
+
+    virtual size_t get_size() const = 0;
+
+    virtual void get_front() const = 0; //TODO add the Iterator
+
+    virtual void get_tail() const = 0; //TODO add the Iterator
+
+    virtual void insert_front(const T& t) = 0;
+
+    virtual void insert_after(const std::shared_ptr<ListNode<T>>& predecessor, const T& t) = 0;
+
+    virtual void remove(const std::shared_ptr<ListNode<T>>& node) = 0;
+};
+
+#include "../../src/doublyLinkedList/doublyLinkedList.tpp"
