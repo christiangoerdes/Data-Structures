d1cd7253378e72b2f1505bfbc2ce14292295b464
Christian Gördes <christian.goerdes@outlook.de>
2023-04-27 23:24:56 +0200

    fixed the ListNode pointer

--- include/linkedList/singlyLinkedList/singlyLinkedListIterator.hpp
+++ include/linkedList/singlyLinkedList/singlyLinkedListIterator.hpp
@@ -4,5 +4,13 @@
 
 template <typename T> // Template declaration for a generic type T
 class SinglyLinkedListIterator : public ListIterator<T>{
+public:
+    SinglyLinkedListIterator(std::shared_ptr<SinglyLinkedListNode<T>> node) : current(node) {}
 
+    std::shared_ptr<SinglyLinkedListNode<T>> operator->() const {
+        return current;
+    }
+
+private:
+    std::shared_ptr<SinglyLinkedListNode<T>> current; // Shared pointer to the current node
 };
