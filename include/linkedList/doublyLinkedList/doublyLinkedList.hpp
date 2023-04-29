a7a154bb6aab8584c7397f6ceba41c954523d0e9
Christian Gördes <christian.goerdes@outlook.de>
2023-04-29 13:11:09 +0200

    corrected both ListInterfaces

--- include/linkedList/doublyLinkedList/doublyLinkedList.hpp
+++ include/linkedList/doublyLinkedList/doublyLinkedList.hpp
@@ -1,33 +1,20 @@
-/**
- * @file abstractDoublyLinkedList.hpp
- * @brief Definition of the AbstractDoublyLinkedList class template and its member functions
- */
-
 #pragma once
 
 #include "doublyLinkedListNode.hpp"
 #include "doublyLinkedListIterator.hpp"
 #include "../linkedList.hpp"
 
-/**
- * @brief An abstract base class for doubly linked lists
- * @tparam T The type of data stored in the nodes of the list
- */
 template <typename T>
 class AbstractDoublyLinkedList : public AbstractLinkedList<T>{
 
 public:
 
-    /**
-     * @brief Returns an iterator to the last element in the list
-     * @return An iterator to the last element in the list
-     */
+    virtual DoublyLinkedListIterator<T> get_front() const = 0;
+
+    virtual void insert_after(const std::shared_ptr<DoublyLinkedListNode<T>>& predecessor, const T& t) = 0;
+
     virtual DoublyLinkedListIterator<T> get_tail() const = 0;
 
-    /**
-     * @brief Removes the specified node from the list
-     * @param node A shared pointer to the node to be removed
-     */
     virtual void remove(const std::shared_ptr<DoublyLinkedListNode<T>>& node) = 0;
 };
 
--- include/linkedList/singlyLinkedList/singlyLinkedList.hpp
+++ include/linkedList/singlyLinkedList/singlyLinkedList.hpp
@@ -1,37 +1,20 @@
 #pragma once
-/**
- * @file singlyLinkedList.hpp
- * @brief Header file for a singly linked list implementation
- */
 
 #include "singlyLinkedListNode.hpp"
 #include "singlyLinkedListIterator.hpp"
 #include "../LinkedList.hpp"
 
-/**
- * @brief Abstract interface for a singly linked list
- * @tparam T Type of data stored in the list
- */
 template <typename T>
 class AbstractSinglyLinkedList : public AbstractLinkedList<T>{
 
 public:
 
-    /**
-     * @brief Get an iterator pointing to the first node in the list
-     * @return Iterator to the first node
-     */
     virtual SinglyLinkedListIterator<T> get_front() const = 0 ;
 
-    /**
-     * @brief Remove the node at the front of the list
-     */
+    virtual void insert_after(const std::shared_ptr<SinglyLinkedListNode<T>>& predecessor, const T& t) = 0;
+
     virtual void remove_front() = 0;
 
-    /**
-     * @brief Remove the node after the given predecessor node
-     * @param predecessor Node after which the next node should be removed
-     */
     virtual void remove_after(const std::shared_ptr<SinglyLinkedListNode<T>>& predecessor) = 0;
 };
 
