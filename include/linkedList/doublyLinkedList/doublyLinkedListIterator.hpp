3e5a9c7c178890c2c8ca1fcd1be06bc924ad29c3
Christian Gördes <christian.goerdes@outlook.de>
2023-04-29 13:32:41 +0200

    refactoring

--- include/linkedList/doublyLinkedList/doublyLinkedListIterator.hpp
+++ include/linkedList/doublyLinkedList/doublyLinkedListIterator.hpp
@@ -7,36 +7,36 @@ class DoublyLinkedListIterator : public ListIterator<T>{
 public:
     DoublyLinkedListIterator(std::shared_ptr<DoublyLinkedListNode<T>> node) : current(node) {}
 
-    virtual T& operator*() const {
+    T& operator*() const {
         return current->get_data();
     }
 
-    virtual std::shared_ptr<ListNode<T>> operator->() const {
+    std::shared_ptr<ListNode<T>> operator->() const {
         return current;
     }
 
-    virtual DoublyLinkedListIterator& operator++() {
+    DoublyLinkedListIterator& operator++() {
         current = current->get_next();
         return *this;
     }
  
-    virtual DoublyLinkedListIterator operator++(int) {
+    DoublyLinkedListIterator operator++(int) {
         DoublyLinkedListIterator temp(current);
         ++(*this);
         return temp;
     }
 
-    virtual DoublyLinkedListIterator operator--(int) {
+    DoublyLinkedListIterator operator--(int) {
         DoublyLinkedListIterator temp(current);
         --(*this);
         return temp;
     }
 
-    virtual bool operator==(const DoublyLinkedListIterator& other) const {
+    bool operator==(const DoublyLinkedListIterator& other) const {
         return current == other.current;
     }
 
-    virtual bool operator!=(const DoublyLinkedListIterator& other) const {
+    bool operator!=(const DoublyLinkedListIterator& other) const {
         return *this != other;
     }
 
