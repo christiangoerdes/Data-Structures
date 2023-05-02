88864c1073564c23983e6d070601cb88728d83fe
Christian Gördes <christian.goerdes@outlook.de>
2023-05-02 10:55:32 +0200

    re-added comments

--- include/linkedList/doublyLinkedList/doublyLinkedListNode.hpp
+++ include/linkedList/doublyLinkedList/doublyLinkedListNode.hpp
@@ -1,35 +1,45 @@
-/**
- * @file doublyLinkedListNode.hpp
- * @brief Implementation of a node for a doubly linked list
- * @author [Insert your name here]
- */
-
 #pragma once
 #include <memory>
 #include "../singlyLinkedList/singlyLinkedListNode.hpp"
 
 /**
- * @brief A class representing a node for a doubly linked list
+ * @brief A node for a doubly linked list.
  *
- * @tparam T The type of data stored in the node
+ * @tparam T The type of data stored in the node.
  */
 template <typename T>
 class DoublyLinkedListNode : public SinglyLinkedListNode<T>{
 
 public:
 
+    /**
+     * @brief Constructor for DoublyLinkedListNode.
+     *
+     * @param newData The data to be stored in the node.
+     */
     DoublyLinkedListNode(T newData) : SinglyLinkedListNode<T>(newData){
         prev = nullptr;
     }
 
+    /**
+     * @brief Returns a shared pointer to the previous node in the list.
+     *
+     * @return std::shared_ptr<DoublyLinkedListNode<T>> A shared pointer to the previous node in the list.
+     */
     std::shared_ptr<DoublyLinkedListNode<T>> get_prev() const {
         return prev;
     }
 
+    /**
+     * @brief Sets the previous node in the list.
+     *
+     * @param prevNode A shared pointer to the previous node in the list.
+     */
     void set_prev(std::shared_ptr<DoublyLinkedListNode<T>> prevNode) {
         prev = prevNode;
     }
 
 private:
-    std::shared_ptr<DoublyLinkedListNode<T>> prev;
+    std::shared_ptr<DoublyLinkedListNode<T>> prev; /**< A shared pointer to the previous node in the list. */
 };
+
--- include/linkedList/singlyLinkedList/singlyLinkedListNode.hpp
+++ include/linkedList/singlyLinkedList/singlyLinkedListNode.hpp
@@ -1,40 +1,58 @@
-/**
- * @file singlyLinkedListNode.cpp
- * @brief Implementation of SinglyLinkedListNode class template.
- */
-
 #pragma once
 #include <memory>
 
 /**
- * @brief A node of a singly linked list that stores an element of type T.
+ * @brief A node for a singly linked list.
  *
- * This class template inherits from ListNode<T>, and adds a next pointer.
- *
- * @tparam T The type of the element stored in the node.
+ * @tparam T The type of data stored in the node.
  */
 template <typename T>
 class SinglyLinkedListNode {
 public:
+    /**
+     * @brief Constructor for SinglyLinkedListNode.
+     *
+     * @param newData The data to be stored in the node.
+     */
     SinglyLinkedListNode(T newData) : data(newData), next(nullptr) {}
 
+    /**
+     * @brief Returns the data stored in the node.
+     *
+     * @return const T& The data stored in the node.
+     */
     virtual const T& get_data() const {
         return data;
     }
 
+    /**
+     * @brief Sets the data stored in the node.
+     *
+     * @param newData The new data to be stored in the node.
+     */
     virtual void set_data(const T& newData) {
         data = newData;
     }
 
+    /**
+     * @brief Returns a shared pointer to the next node in the list.
+     *
+     * @return std::shared_ptr<SinglyLinkedListNode<T>> A shared pointer to the next node in the list.
+     */
     virtual std::shared_ptr<SinglyLinkedListNode<T>> get_next() const {
         return next;
     }
 
+    /**
+     * @brief Sets the next node in the list.
+     *
+     * @param nextNode A shared pointer to the next node in the list.
+     */
     virtual void set_next(const std::shared_ptr<SinglyLinkedListNode<T>> nextNode) {
         next = nextNode;
     }
 
 private:
-    T data;
-    std::shared_ptr<SinglyLinkedListNode<T>> next;
+    T data; /**< The data stored in the node. */
+    std::shared_ptr<SinglyLinkedListNode<T>> next; /**< A shared pointer to the next node in the list. */
 };
