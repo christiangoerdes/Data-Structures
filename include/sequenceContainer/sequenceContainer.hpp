feb8d250ff45955deadf5ba53a970c2b61bb824e
Christian Gördes <christian.goerdes@outlook.de>
2023-05-03 15:19:41 +0200

    corrected comment

--- include/sequenceContainer/sequenceContainer.hpp
+++ include/sequenceContainer/sequenceContainer.hpp
@@ -1,17 +1,18 @@
 #pragma once
 
 /**
- * @brief A class representing the abstract base class for linked list data structures
+ * @brief A class representing the abstract base class sequence container
  *
  * This class defines the interface that must be implemented by any derived linked list classes.
- *
- * @tparam T The type of the elements stored in the linked list
  */
 
 class SequenceContainer {
 
 public:
-
+    /**
+     * @brief Returns the size of the container
+     * @return the size of the container.
+     */
     virtual size_t get_size() const = 0;
 
 };
