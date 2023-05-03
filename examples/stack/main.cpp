cbea7c1962f127ad101fc69193eb140079a489a0
Christian Gördes <christian.goerdes@outlook.de>
2023-05-03 21:06:37 +0200

    improved example stack in main

--- examples/stack/main.cpp
+++ examples/stack/main.cpp
@@ -1,8 +1,15 @@
 #include "../../include/Container/stack/stack.hpp"
 
 int main() {
-    Stack<int> s;
+    Stack<int> my_stack; // Create a stack of integers
 
-    s.push(3);
+    // Push some elements onto the stack
+    my_stack.push(1);
+    my_stack.push(2);
+    my_stack.push(3);
 
+    // Pop elements from the stack and print them
+    while (!my_stack.is_empty()) {
+        std::cout << my_stack.pop() << std::endl;
+    }
 }
