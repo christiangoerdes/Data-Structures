39c9adae239c23f6f7c4b2bb29c69ec0229cd77e
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-05-04 00:57:57 +0200

    implemented additional unit tests

--- tests/queue/queue_test.cpp
+++ tests/queue/queue_test.cpp
@@ -3,9 +3,27 @@
 
 // Unit tests for Queue
 TEST(QueueTest, InsertionAndRemoval) {
-    Queue<std::string> queue;
+    Queue<int> queue;
     ASSERT_EQ(queue.get_size(), 0);
     ASSERT_EQ(queue.is_empty(), true);
+
+    queue.push(5);
+    ASSERT_EQ(queue.pop(), 5);
+
+    queue.push(6);
+    queue.push(7);
+    ASSERT_EQ(queue.pop(), 6);
+    ASSERT_EQ(queue.pop(), 7);
+
+    queue.push(42);
+    ASSERT_EQ(queue.is_empty(), false);
+    ASSERT_EQ(queue.pop(), 42);
+
+    queue.push(33);
+    queue.push(77);
+    queue.push(44);
+    ASSERT_EQ(queue.get_size(), 3);
+
 }
 
 int main(int argc, char **argv) {
