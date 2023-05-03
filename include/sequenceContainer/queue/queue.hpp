88ce9f7a4f5303c24925d8bb157ef170a760085a
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-05-03 17:14:17 +0200

    added comments to queue.hpp

--- include/sequenceContainer/queue/queue.hpp
+++ include/sequenceContainer/queue/queue.hpp
@@ -8,18 +8,24 @@
 #include "../sequenceContainer.hpp"
 #include "../linkedList/doublyLinkedList/doublyLinkedList.hpp"
 
+/**
+ * @brief Abstract base class for Queue
+ *
+ * This class provides an interface for implementing a Queue.
+ *
+ */
 template <typename T>
 class AbstractQueue : public SequenceContainer {
 public:
 
-    /*
+    /**
      * @brief Pops item and returns it
      *
      * @return T An object of the type T
      */
     virtual T pop() const = 0;
 
-    /*
+    /**
      * @brief Pushes new item of type T
      *
      * @param t Item of type T to insert
