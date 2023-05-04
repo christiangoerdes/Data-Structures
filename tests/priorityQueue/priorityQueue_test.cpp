c12fb6afc02e6ed5fe4df5015741843eba2e0e7c
Sergej Zuev <sergejnexus@outlook.com>
2023-05-04 03:13:37 +0200

    Test fertiggeschrieben

--- tests/priorityQueue/priorityQueue_test.cpp
+++ tests/priorityQueue/priorityQueue_test.cpp
@@ -1,5 +1,4 @@
 #include <gtest/gtest.h>
-#include "../../include/container/linkedList/doublyLinkedList/doublyLinkedList.hpp"
 #include "../../include/container/priorityQueue/priorityQueue.hpp"
 
 TEST(PriorityQueueTest, InsertionAndRemoval) {
@@ -11,31 +10,24 @@ ASSERT_EQ(queue.get_size(), 0);
 // Insert an element
 queue.push(1);
 ASSERT_EQ(queue.get_size(), 1);
-ASSERT_EQ(*queue.get_front(), 1);
-ASSERT_EQ(*queue.get_back(), 1);
-
+ASSERT_EQ(queue.top(), 1);
 
 // Insert elements of higher priority
 queue.push(2);
 queue.push(3);
 ASSERT_EQ(queue.get_size(), 3);
-ASSERT_EQ(*queue.get_front(), 3);
+ASSERT_EQ(queue.top(), 3);
 
 // Insert an element of lower priority
 queue.push(0);
-ASSERT_EQ()
+ASSERT_EQ(queue.top(), 3);
 
-// Remove the element after a given node
-iter = list.get_front();
-list.remove(iter.get_pointer() -> get_next());
-ASSERT_EQ(list.get_size(), 1);
-ASSERT_EQ(*list.get_front(), "3");
-ASSERT_EQ(*list.get_end(), "3");
+// Remove elements with highest priority
+queue.pop();
+ASSERT_EQ(queue.top(), 2);
+queue.pop();
+ASSERT_EQ(queue.top(), 1);
 
-// Remove the last element from the list
-list.remove(iter);
-ASSERT_EQ(list.get_size(), 0);
-ASSERT_TRUE(list.is_empty());
 }
 
 int main(int argc, char **argv) {
