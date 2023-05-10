e145e31713549e0231327a74a610a67cdfb18009
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-05-10 22:40:06 +0200

    added more testing

--- examples/unorderedSet/main.cpp
+++ examples/unorderedSet/main.cpp
@@ -1,5 +1,16 @@
 #include "../../include/Container/unorderedSet/unorderedSet.hpp"
 
+#include <iostream>
+
 int main() {
     UnorderedSet<int> x;
+
+    std::cout << x.get_size() << std::endl;
+
+    x.insert(5);
+    x.insert(82);
+    x.remove(5);
+
+    std::cout << x.get_size() << std::endl;
+
 }
